89f1c5f580259cb65af1992f04fb5d58ebbc99d7
Sergio <sherrera725@gmail.com>
2023-03-05 23:04:44 -0800

    add IBOutlet references in GameVC for all UI button elements
    Added qty 9 IBOutlets for UI button elements in GameVC, along with variables for player turn and button array for board spaces.

--- TicTacToe/Controller/GameVC.swift
+++ TicTacToe/Controller/GameVC.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class GameVC: UIViewController {
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var gameBoardBtn1: UIButton!
+    @IBOutlet weak var gameBoardBtn2: UIButton!
+    @IBOutlet weak var gameBoardBtn3: UIButton!
+    @IBOutlet weak var gameBoardBtn4: UIButton!
+    @IBOutlet weak var gameBoardBtn5: UIButton!
+    @IBOutlet weak var gameBoardBtn6: UIButton!
+    @IBOutlet weak var gameBoardBtn7: UIButton!
+    @IBOutlet weak var gameBoardBtn8: UIButton!
+    @IBOutlet weak var gameBoardBtn9: UIButton!
+
+    var playerTurn = "X"
+    var board = [UIButton]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
