782ba73db420118f6d345457a294392e204404b0
Sergio <sherrera725@gmail.com>
2023-03-05 23:11:39 -0800

    add primary game functionality for game and user actions
    Added methods in GameVC for following occurrences:
    - initialize game board with empty spaces
    - reset/clear game board for new game
    - add player move to game board
    - check if all board spaces have been used
    - IBAction for user tapping game board space (button)
    - adding a player move to game board
    - setting game over alert when all board spaces have been played

--- TicTacToe/Controller/GameVC.swift
+++ TicTacToe/Controller/GameVC.swift
@@ -25,8 +25,75 @@ class GameVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initBoard()
+    }
+    
+    func initBoard() {
+        board.append(gameBoardBtn1)
+        board.append(gameBoardBtn2)
+        board.append(gameBoardBtn3)
+        board.append(gameBoardBtn4)
+        board.append(gameBoardBtn5)
+        board.append(gameBoardBtn6)
+        board.append(gameBoardBtn7)
+        board.append(gameBoardBtn8)
+        board.append(gameBoardBtn9)
+    }
+    
+    func isFullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        
+        return true
     }
 
+    @IBAction func gameBoardBtnTapped(_ sender: UIButton) {
+        addMoveToBoard(sender)
+        
+        if isFullBoard() {
+            gameOverAlert(title: "Draw")
+        }
+    }
+
+    // add player move to board
+    func addMoveToBoard(_ sender: UIButton) {
+        // "sender" is UIButton aka button in gameboard space
+        if sender.title(for: .normal) == nil {
+            // valid board space for player move
+            if playerTurn == "X" {
+                sender.setTitle("X", for: .normal)
+                playerTurn = "O"
+                turnLabel.text = "O"
+            } else {
+                sender.setTitle("O", for: .normal)
+                self.playerTurn = "X"
+                self.turnLabel.text = "X"
+            }
+            
+            sender.isEnabled = false
+        }
+    }
+    
+    func gameOverAlert(title: String) {
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(alertController, animated: true)
+    }
+    
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        
+        playerTurn = "X"
+        turnLabel.text = playerTurn
+    }
+    
 
 }
